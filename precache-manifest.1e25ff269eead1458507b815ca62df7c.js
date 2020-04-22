a78234bc5c478ef3e288ac2296761b25a40d3703
KyeongWonLee <kw0226@playerauctionskorea.com>
2020-04-22 11:33:07 +0900

    Updates

---
+++ precache-manifest.1e25ff269eead1458507b815ca62df7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc87b316de89eddd4daa99d9e6dd723e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62188082fe9d79bf8c4",
+    "url": "/static/css/main.a299d279.chunk.css"
+  },
+  {
+    "revision": "d81438a4d598e83349fe",
+    "url": "/static/js/2.8d5eb2b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d5eb2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62188082fe9d79bf8c4",
+    "url": "/static/js/main.62a7d0f2.chunk.js"
+  },
+  {
+    "revision": "48ab0448f6951abbd48b",
+    "url": "/static/js/runtime-main.add13801.js"
+  }
+]);
